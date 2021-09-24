bd1422f84500b0948878c684524f92cd1a1b8abc
raphaelvaz <raphaeltrombetta@gmail.com>
2021-09-23 23:16:53 -0300

    refactor: Use destructing assigment in httpRequest.body

--- src/presentation/controllers/signUp.ts
+++ src/presentation/controllers/signUp.ts
@@ -2,16 +2,29 @@ import { HttpRequest, HttpResponse } from "../protocols/http"
 import { MissingParamError } from '../errors/missingParamError'
 import { badRequest, successRequest } from "../helpers/httpHelper"
 import { Controller } from "../protocols/controller"
+import { FullNameValidator } from "../protocols/fullnameValidator"
 
 export default class SignUpController implements Controller {
+    constructor(
+        private readonly nameValidator: FullNameValidator,
+    ) { }
     handle(httpRequest: HttpRequest): HttpResponse {
-        if (!httpRequest.body.user) {
+        const { user, birthDate } = httpRequest.body
+
+        if (!user) {
             return badRequest(new MissingParamError('user'))
         }
 
-        if (!httpRequest.body.birthday) {
-            return badRequest(new MissingParamError('birthday'))
+        if (!birthDate) {
+            return badRequest(new MissingParamError('birth date'))
         }
+        //VALIDAR SE É UMA STRING...
+        //TEM QUE SER UM NOME VALIDO...
+        //TEM QUE SER UMA DATA DE NASCIMENTO VALIDA...
+
+        // /^[a-zA-Z'- ]+$/
+        // /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/([12][0-9]{3})$/
+
 
         return successRequest({})
     }
