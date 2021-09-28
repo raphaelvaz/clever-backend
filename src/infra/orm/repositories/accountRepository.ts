0f782e6344b2d4989349ef4325f90efa9a3d8245
raphaelvaz <raphaeltrombetta@gmail.com>
2021-09-28 17:44:14 -0300

    feat: ensure typeormAccountRepository implements add method correctly according to the interface

--- src/infra/orm/repositories/accountRepository.ts
+++ src/infra/orm/repositories/accountRepository.ts
@@ -1,16 +1,27 @@
 import { getRepository } from "typeorm";
 import { AccountRepository } from "../../../data/protocols/AccountRepository";
+import { Account } from "../../../domain/entities/account";
 import { AccountRequestData } from "../../../domain/usecases/addAccount";
-import { Account } from '../models/account'
+import { Account as OrmAccount } from '../models/account'
 
 export class TypeormAccountRepository implements AccountRepository {
     async exists({ name, birthDate }: AccountRequestData): Promise<boolean> {
-        const accountRepository = getRepository(Account);
+        const accountRepository = getRepository(OrmAccount);
         const checkIfExists = await accountRepository.findOne({
-            where: [{ name }, { birth_date: birthDate }],
+            where: [{ name }, { birthDate }],
         })
 
         if (checkIfExists) return true;
         return false;
+    }
+
+    async add({ name, birthDate }: AccountRequestData): Promise<Account> {
+        const accountRepository = getRepository(OrmAccount);
+
+        const account = accountRepository.create({ name, birthDate })
+
+        await accountRepository.save(account);
+
+        return account
     }
 }
