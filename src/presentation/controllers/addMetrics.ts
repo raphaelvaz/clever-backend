95e9fb5138f7446f781dfb52a47e3d4a40dcbf8b
raphaelvaz <raphaeltrombetta@gmail.com>
2021-09-29 09:48:48 -0300

    feat: ensure addMetricsController return 400 if metrics length is not six

--- src/presentation/controllers/addMetrics.ts
+++ src/presentation/controllers/addMetrics.ts
@@ -1,9 +1,11 @@
+import { InvalidParamError } from "../errors/invalidParamError";
 import { MissingParamError } from "../errors/missingParamError";
 import { badRequest } from "../helpers/httpHelper";
 import { Controller } from "../protocols/controller";
 import { HttpRequest, HttpResponse } from "../protocols/http";
 
 
+const NUMBER_DAILY_MEASUREMENTS = 6;
 
 export default class AddMetricsController implements Controller {
     constructor() { }
@@ -13,5 +15,7 @@ export default class AddMetricsController implements Controller {
         if (!account_id) return badRequest(new MissingParamError('account_id'))
 
         if (!metrics) return badRequest(new MissingParamError('metrics'))
+        //TODO SERA QUE TEM QUE DA MAIS INFORMAÇÃO DO ERRO...
+        if (metrics.length !== NUMBER_DAILY_MEASUREMENTS) return badRequest(new InvalidParamError('metrics'))
     }
 }
