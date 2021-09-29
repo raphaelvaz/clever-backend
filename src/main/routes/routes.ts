5d94cc1b71e6336cf23f0c238102fd143b1a7db2
raphaelvaz <raphaeltrombetta@gmail.com>
2021-09-29 20:15:10 -0300

    feat: add metrics route

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -2,7 +2,10 @@ import { Router } from 'express'
 import { ExpressAdapter } from '../adapters/expressRouter';
 
 import { addAccountFactory } from '../factories/addAccountFactory';
+import { addMetricFactory } from '../factories/addMetricFactory';
 
 export const routes = Router();
 
 routes.post('/signup', ExpressAdapter(addAccountFactory()));
+
+routes.post('/metrics', ExpressAdapter(addMetricFactory()))
