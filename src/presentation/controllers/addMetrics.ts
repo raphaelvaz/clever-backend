049f9dfa5572a1541fb30ad5144abe75976db877
raphaelvaz <raphaeltrombetta@gmail.com>
2021-09-29 10:35:19 -0300

    feat: ensure addMetricsController return 400 if a invalid metric date is provider

--- src/presentation/controllers/addMetrics.ts
+++ src/presentation/controllers/addMetrics.ts
@@ -4,18 +4,37 @@ import { badRequest } from "../helpers/httpHelper";
 import { Controller } from "../protocols/controller";
 import { HttpRequest, HttpResponse } from "../protocols/http";
 
+interface HttpRequestBody {
+    account_id: string;
+    metrics: Metric[];
+}
+
+interface Metric {
+    date: Date;
+    bpm: number;
+    pamin: number;
+    pamax: number;
+}
 
 const NUMBER_DAILY_MEASUREMENTS = 6;
+const HOURS_AVAILABLE = ['02:00:00', '06:00:00', '10:00:00', '14:00:00', '18:00:00', '22:00:00']
 
 export default class AddMetricsController implements Controller {
     constructor() { }
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        const { account_id, metrics } = httpRequest.body
+        const { account_id, metrics } = httpRequest.body as HttpRequestBody
 
         if (!account_id) return badRequest(new MissingParamError('account_id'))
 
         if (!metrics) return badRequest(new MissingParamError('metrics'))
-        //TODO SERA QUE TEM QUE DA MAIS INFORMAÇÃO DO ERRO...
+        //TODO TER MAIS INFORMAÇÃO DO ERRO...
         if (metrics.length !== NUMBER_DAILY_MEASUREMENTS) return badRequest(new InvalidParamError('metrics'))
+        //new Date("12/12/1996 07:05:45").toLocaleTimeString() === '07:05:45'
+        metrics.forEach((metric: Metric) => {
+            if (!HOURS_AVAILABLE.includes(metric.date.toLocaleTimeString()))
+                return badRequest(new InvalidParamError('metric date'))
+        })
+
+
     }
 }
